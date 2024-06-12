f258e9d1c6c783d1a459e4ccc9538ba2492f3568
mmd <44922129+MohmdJD@users.noreply.github.com>
2024-06-12 12:12:47 +0330

    betas-5

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def doctor():
     global datas
     print("\nbe menu pezeshkan khosh omadid\ngozineh morde nazre khod ra entekhab konid")
     print("\n\t\t1- ezafe kardn pezeshk jadid\n\t\t2- hazf pezeshk\n\t\t3- viraish pezeshk\n\t\t4- joste'joi pezeshk\n\t\t5- list pezeshk\n\t\t6- joste'joi takhasos\n\t\t7- main menu↩")
-    Doctor_Option = input()
+    Doctor_Option = input(Fore.LIGHTYELLOW_EX + ">" + Fore.RESET)
     match Doctor_Option:
         case '1':
             isNew = str(input("pezeshk jadid\n\ncode pezesh ra vard konid:\t"))
@@ -46,7 +46,7 @@ def doctor():
                 if total_checker("string", name):
                     datas += name + ","
                 else:
-                    print(Fore.RED + "\nname pezesh bayad shamel horof bashad bashad\n" + Fore.RESET)
+                    print(Fore.RED + "\nname pezesh bayad shamel horof bashad bashad [A-Z/a-z/-/'/.]\n" + Fore.RESET)
                     doctor()
 
                 phone = str(input("\nmobile pezesh ra vard konid:\t"))
@@ -81,9 +81,11 @@ def doctor():
                 if results:
                     with open("doctors.txt", "r+") as file:
                         lines = file.readlines()
+                        file.close()
                     lines = list(filter(lambda line: line not in results, lines))
                     with open("doctors.txt", "w+") as file:
                         file.writelines(lines)
+                        file.close()
 
                     headers = ["code", "name", "mobile", "specialist"]
                     for result in results:
@@ -110,16 +112,18 @@ def doctor():
                 if results:
                     with open("doctors.txt", "r+") as file:
                         lines = file.readlines()
+                        file.close()
                     lines = list(filter(lambda line: line not in results, lines))
                     with open("doctors.txt", "w+") as file:
                         file.writelines(lines)
+                        file.close()
 
                     datas = code + ","
                     name = str(input("\nname jadide pezesh ra vard konid:\t"))
                     if total_checker("string", name):
                         datas += name + ","
                     else:
-                        print(Fore.RED + "\nname pezesh bayad shamel horof bashad bashad\n" + Fore.RESET)
+                        print(Fore.RED + "\nname pezesh bayad shamel horof bashad bashad [A-Z/a-z/-/'/.]\n" + Fore.RESET)
                         doctor()
 
                     phone = str(input("\nmobile jadid pezesh ra vard konid:\t"))
@@ -139,8 +143,20 @@ def doctor():
                         doctor()
 
                     TouchMyfile("doctors.txt", f"\n{datas}\n")
-
                     empty_lines("doctors.txt")
+
+                    print("Original " + Fore.LIGHTCYAN_EX + code + Fore.RESET + " information:\n")
+                    headers = ["code", "name", "mobile", "specialist"]
+                    for result in results:
+                        values = result.split(',')
+                        for header, value in zip(headers, values):
+                            print(Fore.LIGHTWHITE_EX + Style.DIM + f"-{header}: " + Style.RESET_ALL + Fore.RESET + Fore.LIGHTRED_EX + f"{value}" + Fore.RESET)
+                    print("\n")
+                    print("Updated " + Fore.LIGHTCYAN_EX + code + Fore.RESET + " information:\n")
+                    headers = ["code", "name", "mobile", "specialist"]
+                    for header, value in zip(headers, datas.split(',')):
+                        print(Fore.LIGHTWHITE_EX + Style.DIM + f"-{header}: " + Fore.RESET + Style.RESET_ALL + Fore.LIGHTGREEN_EX + f"{value}" + Fore.RESET)
+                    print("\n")
 
                     doctor()
 
@@ -163,7 +179,7 @@ def doctor():
                         values = result.split(',')
                         for header, value in zip(headers, values):
                             print(Fore.LIGHTCYAN_EX + f"-{header}: " + Fore.RESET + Style.DIM + Fore.LIGHTRED_EX + f"{value}" + Style.RESET_ALL + Fore.RESET)
-                            print("\n")
+                        print("\n")
 
                     doctor()
 
@@ -209,7 +225,7 @@ def mariz():
     print("\nbe menu mariz khosh omadid\ngozineh morde nazre khod ra entekhab konid")
     print("\n\t\t1- ezafe kardn bimar\n\t\t2- hazfe bimar\n\t\t3- viraish etelaate bimar\n\t\t4- joste'joi bimarhaye ye doctor\n\t\t5- list bimaran\n\t\t6- main menu↩")
 
-    Mariz_gozineh = input()
+    Mariz_gozineh = input(Fore.LIGHTYELLOW_EX + ">" + Fore.RESET)
 
     match Mariz_gozineh:
         case '1':
@@ -225,7 +241,13 @@ def mariz():
                         print(Fore.RED + "name bimar bayad shamel horof bashad" + Fore.RESET)
                         mariz()
 
-                    datas += str(input("\npezeshke bimar ra vard konid:\t"))
+                    code_pezeshk =  str(input("\npezeshke bimar ra vard konid:\t"))
+                    if total_checker("integer", code_pezeshk):
+                        if findIsUser("doctors.txt", code_pezeshk) == "yes":
+                            datas += code_pezeshk + ";"
+                        else:
+                            print(Fore.RED + "\npezeshk mojod nist\n" + Fore.RESET)
+                            mariz()
 
                     TouchMyfile("marizan.txt", f"{datas}\n")
 
@@ -247,9 +269,11 @@ def mariz():
                 if results:
                     with open("marizan.txt", "r+") as file:
                         lines = file.readlines()
+                        file.close()
                     lines = list(filter(lambda line: line not in results, lines))
                     with open("marizan.txt", "w+") as file:
                         file.writelines(lines)
+                        file.close()
 
                     headers = ["code", "name", "doctor"]
                     for result in results:
@@ -269,7 +293,58 @@ def mariz():
                 mariz()
 
         case '3':
-            print("delete mariz")
+            code = str(input("\ncode meli bimar ra vard konid:\t"))
+            if total_checker("code_meli", code):
+                results = search_db("marizan.txt", search_value=code)
+                if results:
+                    with open("marizan.txt", "r+") as file:
+                        lines = file.readlines()
+                        file.close()
+                    lines = list(filter(lambda line: line not in results, lines))
+                    with open("marizan.txt", "w+") as file:
+                        file.writelines(lines)
+                        file.close()
+
+                    datas = code + ","
+                    name = str(input("\nname jadide bimar ra vard konid:\t"))
+                    if total_checker("string", name):
+                        datas += name + ","
+                    else:
+                        print(Fore.RED + "\nname bimar bayad shamel horof bashad bashad [A-Z/a-z/-/'/.]\n" + Fore.RESET)
+                        mariz()
+
+                    code_pezeshk = str(input("\npezeshke bimar ra vard konid:\t"))
+                    if total_checker("integer", code_pezeshk):
+                        if findIsUser("doctors.txt", code_pezeshk) == "yes":
+                            datas += code_pezeshk + ";"
+                            TouchMyfile("marizan.txt", f"\n{datas}\n")
+                            empty_lines("marizan.txt")
+                        else:
+                            print(Fore.RED + "\npezeshk mojod nist\n" + Fore.RESET)
+                            mariz()
+
+                    print("Original " + Fore.LIGHTCYAN_EX + code + Fore.RESET + " information:\n")
+                    headers = ["code meli", "name", "doctor"]
+                    for result in results:
+                        values = result.split(',')
+                        for header, value in zip(headers, values):
+                            print(Fore.LIGHTWHITE_EX + Style.DIM + f"-{header}: " + Style.RESET_ALL + Fore.RESET + Fore.LIGHTRED_EX + f"{value}" + Fore.RESET)
+                    print("\n")
+                    print("Updated " + Fore.LIGHTCYAN_EX + code + Fore.RESET + " information:\n")
+                    headers = ["code meli", "name", "doctor"]
+                    for header, value in zip(headers, datas.split(',')):
+                        print(Fore.LIGHTWHITE_EX + Style.DIM + f"-{header}: " + Fore.RESET + Style.RESET_ALL + Fore.LIGHTGREEN_EX + f"{value}" + Fore.RESET)
+                    print("\n")
+
+                    mariz()
+
+                else:
+                    print(Fore.MAGENTA + "\nbimar mojod nist\n" + Fore.RESET)
+                    mariz()
+
+            else:
+                print(Fore.RED + "\ncode meli bayad adade 10 raghami bashad\n" + Fore.RESET)
+                mariz()
 
         case '4':
             print("serch marizhaye doctor")
@@ -296,20 +371,19 @@ def TouchMyfile(Path, datas):
 def findIsUser(file, value):
     with open(file, "r") as file:
         lines = file.readlines()
-
+        file.close()
     for line_number, line in enumerate(lines):
         values = line.strip().split(',')
         if value in values:
             return "yes"
 
     return "no"
-
 def total_checker(type, value):
     patterns = {
         'code_meli': r'^\d{10}$',
         'mobile': r'^09\d{9}$',
         'integer': r'^\d+$',
-        'string': r'^[a-zA-Z\s\-\'\.]+$'
+        'string': r'^[a-zA-Z \-\'\.]+$'
     }
 
     if re.match(patterns[type], value):
@@ -324,6 +398,7 @@ class Specialist(Enum):
 def search_db(filename, search_index=None, search_value=None):
     with open(filename, "r") as file:
         lines = file.readlines()
+        file.close()
 
     results = []
 
@@ -340,17 +415,19 @@ def search_db(filename, search_index=None, search_value=None):
     if results:
         return results
 
+
 def pretty_print_lists_and_search(filename=None, array=None):
     if filename:
         with open(filename, "r") as file:
             file_lines = file.readlines()
+            file.close()
         file_lines = sorted(file_lines, key=lambda x: x.strip().split(',')[2])
     else:
         file_lines = array
 
     if filename == "marizan.txt":
-        headers = ["No", "code", "name", "doctor"]
-        column_widths = [4, 12, 14, 6]
+        headers = ["No", "code meli", "name", "doctor"]
+        column_widths = [4, 12, 14, 26]
     elif filename == "doctors.txt" or (array and not filename):
         headers = ["No", "code", "name", "mobile", "specialist"]
         column_widths = [4, 12, 14, 14, 12]
@@ -376,12 +453,13 @@ def empty_lines(filename):
     if filename:
         with open(filename, "r") as file:
             lines = file.readlines()
-
+            file.close()
         lines = [line for line in lines if line.strip()]
 
         with open(filename, "w") as file:
             file.writelines(lines)
+            file.close()
 
 
 main()
-#color input / output viraish / viraish mariz / empty lines in txt file / close file
+#sorte list ha a-z / fix ",") / fix ; list and outputs / fix virayesh mariz to not delete /
